045556cabeb86df0993967cd6b949dfef7084942
hoangtc <hoang.tran@getz.co>
2023-01-23 09:14:16 +0700

    init popup

---
+++ src/Apps/Online/wwwroot/js/popup.js
@@ -0,0 +1,64 @@
+﻿const $popup = $('#popup');
+
+function hidePopup(callback = null) {
+    $('body').removeAttr('style');
+    $popup.find('.popup').removeClass("popup-show popup-hide");
+    $popup.hide();
+
+    removeProductOpenClass();
+    if (callback != null && callback != undefined) {
+        callback();
+    }
+}
+
+function showPopup(popupSelected, callback = null) {
+    $('body').css('overflow', 'hidden');
+    $(popupSelected).removeClass('popup-hide').addClass('popup-show');
+    $popup.show();
+
+    if (callback != null && callback != undefined) {
+        callback();
+    }
+}
+
+$(function () {
+    $popup.on('click', '.popup-close, .swipe-popup-down', function () {
+        const $currentPopup = $(this).parents('.popup');
+        $currentPopup.addClass("popup-hide").removeClass("popup-show");
+
+        setTimeout(function () {
+            hidePopup();
+        }, 300);
+    });
+
+    $popup.on('click', '.popup-backdrop', function () {
+        hidePopup();
+    });
+
+    $popup.on('click', '.btn-view-cart', function () {
+        hidePopup();
+        $("#my-cart-count").trigger("click");
+    });
+
+    let deviceWindowWidth = $(window).outerWidth();
+    $(window).resize(function () {
+        const currentDeviceWindowWidth = $(window).outerWidth();
+        if (currentDeviceWindowWidth != deviceWindowWidth) {
+            if ((currentDeviceWindowWidth > 991 && deviceWindowWidth < 992)
+                || (currentDeviceWindowWidth < 992 && deviceWindowWidth > 991)) {
+                deviceWindowWidth = currentDeviceWindowWidth;
+                hidePopup();
+            }
+        }
+    });
+
+    $('.swipe-popup-down').each(function () {
+        var mc = new Hammer(this);
+        mc.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+
+        mc.on("swipedown", function (e) {
+            $('.swipe-popup-down').trigger('click');
+        });
+    });
+
+});
---
+++ src/Apps/Online/wwwroot/js/product-detail.js
@@ -0,0 +1,14 @@
+﻿
+
+const removeProductOpenClass = () => {
+    $('.js-product-detail').removeClass('product-open');
+};
+
+$(function () {
+    $('.js-product-detail').on('click', (e) => {
+        removeProductOpenClass();
+        console.log($(e.currentTarget));
+        $(e.currentTarget).addClass('product-open');
+        showPopup('#id');
+    })
+});
